4e6976c90c1a97b732986b1c4672855429d5f148
SergeiStar <teiskava@gmail.com>
2016-10-15 12:56:26 +0400

    add Calculate sum and proizv

--- Calculate.java
+++ Calculate.java
@@ -3,7 +3,12 @@ public class Calculate {
 	public static void main(String[] arg) {
 		
 		System.out.println("sdfsdf..");
-
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int sum = first + second;
+		System.out.println("sum " + sum);
+		int proizv = first * second;
+		System.out.println("proizv " + proizv);
 	}
 
 }
